e35c5176f5c81a198f4f0726def709397ed90da7
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-10-06 09:50:48 -0400

    feat: add template to url hello-world

--- my_first_project/my_first_project/views.py
+++ my_first_project/my_first_project/views.py
@@ -1,9 +1,23 @@
 from django.http import HttpResponse
+from django.template import Template, Context
 from datetime import datetime
 
+route_base = "/home/carlos/work/projects/python/frameworks/django/course_1"
 
 def hello(req):
-    return HttpResponse("Hello World")
+
+    home_html = route_base + "/my_first_project/my_first_project/templates/home.html"
+    document = open(home_html)
+
+    plt = Template(document.read())
+
+    document.close()
+
+    ctx = Context()
+
+    docs = plt.render(ctx)
+
+    return HttpResponse(docs)
 
 def bye(req):
     return HttpResponse('Bye bye')
